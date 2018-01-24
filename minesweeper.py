4c162ed6e352f17a6e414706ab02c9e13510e312
William Stevenson <wstevenson@Williams-MacBook-Pro.local>
2018-01-24 15:48:33 -0800

    initial class structures defined

---
+++ minesweeper.py
@@ -0,0 +1,33 @@
+import random
+import sys
+import os
+
+
+
+class Cell:
+	def __init__(self, state, hidden, value, x, y):
+		self.state = state #Boolean
+		self.hidden = hidden # Boolean
+		self.value
+		self.x = x
+		self.y = y
+
+class Minesweeper:
+	def __init__(self, width, height):
+		self.width = int(width)
+		self.height = int(height)
+		self.is_game_over = False
+
+		self.board = [[Cell(True, True, 0, x, y) for x in range(width)] for y in range(self.height)]
+
+		self.__randomize_mines(self, (self.width*self.height)//4):
+
+
+	def __randomize_mines(self, mines):
+
+		mines_left = mines
+
+
+	def __calculate_numbers(self):
+
+
