6a846b78f1b7519c0b3e080a4603b6a9a35012d9
William Stevenson <wstevenson@Williams-MacBook-Pro.local>
2018-01-24 18:53:52 -0800

    implemented method to create mines and print board

--- minesweeper.py
+++ minesweeper.py
@@ -6,31 +6,65 @@ import os
 
 class Cell:
 	def __init__(self, state, hidden, value, x, y):
-		self.state = state #Boolean
+		self.is_numbered = state #Boolean
 		self.hidden = hidden # Boolean
-		self.value
+		self.value = value
 		self.x = x
 		self.y = y
+
+	def cell_to_string(self):
+		if self.hidden:
+			return "-"
+		else:
+			return str(self.value)
 
 class Minesweeper:
 	def __init__(self, width, height):
 		self.width = int(width)
 		self.height = int(height)
 		self.is_game_over = False
+		self.adjacents = [(-1,1),(0,1),(1,1),
+						(-1,0),			(1,0),
+						(-1,-1),(0,-1),(1,-1)]
 
-		self.board = [[Cell(True, True, 0, x, y) for x in range(width)] for y in range(self.height)]
+		self.board = [[Cell(True, False, 0, x, y) for x in range(width)] for y in range(self.height)]
 
-		self.__randomize_mines(self, (self.width*self.height)//4):
+		self.__randomize_mines((self.width*self.height)//4)
+		self.print_board()
 
 
 	def __randomize_mines(self, mines):
+		mines_left = mines
+
+		while(mines_left > 0):
+			x = random.choice(range(self.width))
+			y = random.choice(range(self.height))
+
+			if self.board[x][y].is_numbered:
+				self.board[x][y].is_numbered = False # Now a mine
+				self.board[x][y].value = -1
+				mines_left -= 1
+			else:
+				continue
 
 	def __calculate_numbers(self):
-
-	def __uncover_blanks(self, x, y):
+		pass
 
 	def reveal_cell(self, x, y):
+		pass
+
+	def __uncover_blanks(self, x, y):
+		pass
 
 	def print_board(self):
+		for y in range(self.height):
+			row_string = " "
+			print(row_string.join([self.board[x][y].cell_to_string() for x in range(self.width)]))
+
+
+if __name__ == "__main__":
+
+	board = Minesweeper(4,4)
+
 
 
